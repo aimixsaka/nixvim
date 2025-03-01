75408228444b6b838f633110bbc4f9ad648e6adc
aimixsaka <aimixsaka@gmail.com>
2025-03-01 15:42:25 +0800

    feat: use 'after' file to override options

--- config/file.nix
+++ config/file.nix
@@ -1,9 +1,13 @@
 {
-  files."after/ftplugin/c.lua" = {
-    opts = {
+  files = {
+    "after/ftplugin/c.lua".opts = {
       tabstop = 8;
       shiftwidth = 8;
       expandtab = false;
+    };
+    "after/ftplugin/fish.lua".opts = {
+      tabstop = 4;
+      shiftwidth = 4;
     };
   };
 }
